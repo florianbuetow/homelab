16161bfd79e92d8a22b80fdb96ea57dc011e10f0
florianbuetow <2320560+florianbuetow@users.noreply.github.com>
2024-05-30 21:39:31 +0200

    Adding input of dynamic ip and node number

--- src/setup_new_pi.py
+++ src/setup_new_pi.py
@@ -5,25 +5,28 @@ from recipes.Recipes import RaspberianBullsEyeK3Recipe
 
 if __name__ == '__main__':
 
+    NETWORK_PREFIX = '192.168.1.'
     TEMPLATE_DIR = '../templates/'
     templateHelper = TemplateHelper(TEMPLATE_DIR)
 
-    dynamic_ip = str(input("Enter the dynamic IP address: ")).strip()
+    dynamic_ip = str(input("Enter the dynamic IP address {}.".format(NETWORK_PREFIX))).strip()
     node_number = int(input("Enter the node number for this host: "))
+    if node_number < 1 or node_number > 254:
+        raise ValueError("Node number must be between 1 and 254")
+
     params = {
-        'USERNAME': 'default_user',
-        'PASSWORD': 'default_pass',
+        'USERNAME': 'pi',
+        'PASSWORD': 'pi',
         'DYNAMIC_IP': dynamic_ip,
-        'STATIC_IP': '192.168.1.' + str(130 + node_number),
+        'STATIC_IP': NETWORK_PREFIX + str(130 + node_number - 1),
         'NETMASK': '24',
-        'GATEWAY_IP': '192.168.1.1',
+        'GATEWAY_IP': NETWORK_PREFIX + '1',
         'DNS_IP': '8.8.8.8',
-        'PINAS_IP': '192.168.1.111',
+        'PINAS_IP': NETWORK_PREFIX + '199',
     }
     for id in range(20):
-        key = "CLUSTER_NODE-%02d_IP'" % (id + 1)
-        params[key] = '192.168.1.' + str(130 + id)
+        key = "CLUSTER_NODE-%02d_IP'" % (id)
+        params[key] = NETWORK_PREFIX + str(130 + id - 1)
 
-    #pprint(params)
-    recipe = RaspberianBullsEyeK3Recipe(templateHelper, )
+    recipe = RaspberianBullsEyeK3Recipe(templateHelper, params)
     recipe.apply()
