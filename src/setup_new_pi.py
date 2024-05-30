aa9c3b76df9b4a9789c130bb248c3a7c72e9bbc5
florianbuetow <2320560+florianbuetow@users.noreply.github.com>
2024-05-30 15:17:48 +0200

    Adding node ip's to prams

--- src/setup_new_pi.py
+++ src/setup_new_pi.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from util.TemplateHelper import TemplateHelper
 from recipes.Recipes import RaspberianBullsEyeK3Recipe
 
@@ -5,13 +7,23 @@ if __name__ == '__main__':
 
     TEMPLATE_DIR = '../templates/'
     templateHelper = TemplateHelper(TEMPLATE_DIR)
-    recipe = RaspberianBullsEyeK3Recipe(templateHelper, {
+
+    dynamic_ip = str(input("Enter the dynamic IP address: ")).strip()
+    node_number = int(input("Enter the node number for this host: "))
+    params = {
         'USERNAME': 'default_user',
         'PASSWORD': 'default_pass',
-        'DYNAMIC_IP': '192.168.1.234',
-        'STATIC_IP': '192.168.1.123',
+        'DYNAMIC_IP': dynamic_ip,
+        'STATIC_IP': '192.168.1.' + str(130 + node_number),
         'NETMASK': '24',
-        'GATEWAY_IP': '192.168.1.100',
+        'GATEWAY_IP': '192.168.1.1',
         'DNS_IP': '8.8.8.8',
-    })
+        'PINAS_IP': '192.168.1.111',
+    }
+    for id in range(20):
+        key = "CLUSTER_NODE-%02d_IP'" % (id + 1)
+        params[key] = '192.168.1.' + str(130 + id)
+
+    #pprint(params)
+    recipe = RaspberianBullsEyeK3Recipe(templateHelper, )
     recipe.apply()
